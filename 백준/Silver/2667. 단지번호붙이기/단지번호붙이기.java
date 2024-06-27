a9afa19b399ba251e336400a22e8c1689e013ea0
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-06-27 11:01:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    static int n, m, result, count;
+    static char[][] graph;
+    static int[] dy = { -1, 1, 0, 0 };
+    static int[] dx = {  0, 0,-1, 1 };
+    
+    // DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+    static void dfs(int y, int x) {
+        
+        // 해당 노드 방문 처리
+    	graph[y][x] = '0'; 
+        count++;
+    	
+        for (int d = 0; d < 4; d++) {
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            
+            if( ny < 0 || nx < 0 || ny >= n || nx >= n || graph[ny][nx] != '1') continue;
+            dfs(ny, nx);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // N, M을 공백을 기준으로 구분하여 입력 받기
+        n = Integer.parseInt(br.readLine());
+
+        graph = new char[n][];
+        
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            graph[i] = br.readLine().toCharArray();
+        }
+        // 모든 노드(위치)에 대하여 음료수 채우기
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+            	count = 0;
+                if( graph[i][j] == '1') {
+                    dfs(i, j);
+                    arr.add(count);
+                    result++;
+                }
+            }
+        }
+        Collections.sort(arr);
+        System.out.println(result);
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
