955f22696b5c3406fc9cd0cfd66317bdcb365a3f
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-06-27 15:42:04 +0900

    [Silver I] Title: Z, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N,r,c, ans;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		N = (int) Math.pow(2,N); 
+		
+		int y = 0;
+		int x = 0;
+		
+		// (r,c) 위치까지 찾아질 동안 계속 분할 처리
+		while(true) {
+			if(N == 1)break;
+			
+			N /= 2;
+			
+			if( r < y + N && c < x + N) {
+				; // 분할 영역
+			}
+			else if( r < y + N && c >= x + N) {
+				ans += N * N;
+				x += N;
+			}else if( r >= y + N && c < x + N) {
+				ans += N * N * 2;
+				y += N;
+			}else {
+				ans += N * N * 3;
+				y += N;
+				x += N;
+			}
+		}
+		System.out.println(ans);
+	}
+
+}
