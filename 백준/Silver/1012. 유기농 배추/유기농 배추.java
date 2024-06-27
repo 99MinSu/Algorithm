f419d7c0a62a19aee749656432b89558acfefdba
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-06-27 21:59:25 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 public class Main {
     static int M, N, K;
     static int[][] map;
-    static boolean[][] visited;
     static int[] dx = {0, 0, -1, 1}; // 좌우 이동
     static int[] dy = {-1, 1, 0, 0}; // 상하 이동
 
@@ -20,7 +19,6 @@ public class Main {
             K = Integer.parseInt(st.nextToken());
 
             map = new int[M][N];
-            visited = new boolean[M][N];
 
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -32,7 +30,7 @@ public class Main {
             int count = 0;
             for (int i = 0; i < M; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (map[i][j] == 1 && !visited[i][j]) {
+                    if (map[i][j] == 1) {
                         dfs(i, j);
                         count++;
                     }
@@ -44,14 +42,14 @@ public class Main {
     }
 
     static void dfs(int x, int y) {
-        visited[x][y] = true;
+        map[x][y] = 0;
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
             if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
-                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                if (map[nx][ny] == 1) {
                     dfs(nx, ny);
                 }
             }
