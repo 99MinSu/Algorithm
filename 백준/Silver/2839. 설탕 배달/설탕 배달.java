9f9785eb3727bb47975c6a648d346a54513819a3
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-01 09:42:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Main {
-	static int N, count;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+public class Main { 
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
         
-		while(N>0) {
-			if(N % 5 == 0) {
-				count += N/5; 
-				break;
-			}else {
-				N -= 3;
-				count++;
+		if (N >= 3)
+			dp[3] = 1;
+		if (N >= 5)
+			dp[5] = 1;
+		
+        for (int i = 6; i <= N; i++) {
+			if (i % 5 == 0) {
+				dp[i] = dp[i - 5] + 1;
+			} else if (i % 3 == 0) {
+				dp[i] = dp[i - 3] + 1;
+			} else {
+				if (dp[i - 3] != 0 && dp[i - 5] != 0) {
+					dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+				}
 			}
-			if(N < 0) {
-				count = -1;
-			}
-			
 		}
 		
-		System.out.println(count);
+        if (dp[N] == 0) {
+			System.out.println("-1");
+			return;
+		}
+		System.out.println(dp[N]);
 	}
-
 }
