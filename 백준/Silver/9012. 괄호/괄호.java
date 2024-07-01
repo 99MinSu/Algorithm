9c69e74f8fbfb564ff6f90135c223cb00df2452a
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-01 10:23:03 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,33 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
+	static int n, count;
+	static String ans;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			String S = br.readLine();
-			
-			//문자열의 각 자리의 값을 확인할때는 char형태로 바꿔서 확인하기 때문에 스택도 Character형으로 해준다.
-			Stack<Character> stack = new Stack<Character>();
-			
-			for(int j = 0; j < S.length(); j++) {
-				if(S.charAt(j) == '(') {
-					stack.push(S.charAt(j));
-				}else {
-					//비어있는지를 먼저 본다.
-					if(stack.empty()) {
-						stack.push(S.charAt(j));
-						break;
-					}else {
-						stack.pop();
-					}
+		n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			String s = br.readLine();
+			count = 0;
+			for (int j = 0; j < s.length(); j++) {
+				if(s.charAt(j) == '(') {
+					count++;
+				}else if(s.charAt(j) == ')') {
+					count--;
+				}
+				if(count < 0) {
+					ans = "NO";
+					break;
 				}
 			}
-			if(stack.empty()) System.out.println("YES");
-			else System.out.println("NO");
+			if(count == 0) {
+				ans = "YES";
+			}else
+				ans = "NO";
+			System.out.println(ans);
 		}
-	}
 
+	}
+ 
 }
