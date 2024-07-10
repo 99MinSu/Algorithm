88f80756d4c39cfcc746ea22356b02fc51a90752
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-10 16:50:55 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n,k;
+	public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+	    n = Integer.parseInt(st.nextToken());
+	    k = Integer.parseInt(st.nextToken());
+	    boolean[] check = new boolean[n+1];
+	    	
+	    int cnt = 0;
+	    for(int i = 2; i<=n; i++) {
+	    	for(int j = i; j<=n; j+=i) {
+	    		if(check[j] == false) {
+	        		cnt++;
+	        		check[j] = true;
+	    		}
+	    		if(cnt == k) {
+	    			System.out.println(j);
+	    			return;
+	    		}
+	    	}
+	    }
+	}
+}
