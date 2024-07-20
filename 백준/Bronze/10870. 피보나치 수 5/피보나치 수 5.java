836db16db804f1862ffeabd887957209576a35e0
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-20 21:57:56 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -2,23 +2,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int[] dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N + 1];  //DP 배열 초기화
-        
-        for (int i = 0; i <= N; i++) {
-        	if(i == 0) dp[0] = 0;
-        	else if(i == 1) dp[1] = 1;
-        	else dp[i] = dp[i-1] + dp[i-2];  //피보나치 공식 이용
-        }
-        System.out.print(dp[N]);
+        dp = new int[N + 1];  //DP 배열 초기화
+                
+        System.out.print(fibo(N));
     }
-
-    // 재귀로 푸는 법
+    // 탑다운 방식
 	static int fibo(int N) {
-		if (N == 0)	return 0;
-		if (N == 1)	return 1;
-		return fibo(N - 1) + fibo(N - 2);
+		if (N <= 1)	return N;
+		if (dp[N] != 0)	return dp[N]; // 메모이제이션
+		
+		dp[N] = fibo(N-1) + fibo(N-2);
+		return dp[N];
 	}
 }
