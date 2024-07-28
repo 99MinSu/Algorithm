4b0c64afd0e50d0cf09c34fd639265fe3e96c4ac
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-28 16:39:22 +0900

    [Gold V] Title: 동전, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 0; tc < T; tc++) {
+        	
+        	int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] cost = new int[N];
+            for (int i = 0; i < N; i++) {
+				cost[i] = Integer.parseInt(st.nextToken());
+			}
+            
+            int M = Integer.parseInt(br.readLine());
+            
+            int[] dp = new int[M+1];
+           
+            dp[0] = 1;
+           
+            for (int i = 0; i < N; i++) {
+    			for (int j = cost[i]; j <= M; j++) {
+    				dp[j] += dp[j-cost[i]];
+    			}
+    		}
+            
+            System.out.println(dp[M]);
+        }
+    }
+}
