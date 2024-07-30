9fbd185d6e3b1d6b8044ef3c70c63ab012a57872
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-07-30 21:18:49 +0900

    [level 1] Title: 예산, Time: 1.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,18 +1,12 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        
         Arrays.sort(d);
-        
-        int sum = 0;
-            
-        for(int i = 0; i < d.length; i++) {
-            sum += d[i];
-            if(sum > budget){
+        for(int i = 0; i < d.length; i++){
+            budget -= d[i];
+            if(budget < 0)
                 break;
-            }
             answer++;
         }
         return answer;
