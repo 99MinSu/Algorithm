87bcdde8837d1c42ad6f32d595b4a6e8a1adbf13
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-08-16 22:34:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 112.16 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,21 @@
-import java.util.*;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String, Integer> hm = new HashMap<>();
         
-        for(int i = 0; i < completion.length; i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
-            }            
+        for (String player : participant)
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+            
+        for (String player : completion)
+            hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
         }
-        
-        return participant[participant.length-1];
+        return answer;
     }
 }
