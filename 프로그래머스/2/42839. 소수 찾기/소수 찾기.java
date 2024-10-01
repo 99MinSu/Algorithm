a636ad66968a2aa276780a0dca3a5878ba0fc5a5
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-10-01 16:28:18 +0900

    [level 2] Title: 소수 찾기, Time: 23.10 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,42 +1,45 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    static ArrayList<Integer> arr = new ArrayList<>();
-    static boolean[] check = new boolean[7];
+    static HashSet<Integer> set = new HashSet<>(); // 중복값 제거 위한 set
+    static char[] arr; // 종이조각
+    static boolean[] visited; // 사용여부
     
     public int solution(String numbers) {
         int answer = 0;
+        arr = new char[numbers.length()];
+        visited = new boolean[numbers.length()];
+        
         for(int i=0; i<numbers.length(); i++){
-            dfs(numbers,"",i+1);
+            arr[i] = numbers.charAt(i); 
         }
+                
+        dfs("",0); // 재귀함수
         
-        for(int i=0; i<arr.size(); i++){
-            if(isPrime(arr.get(i))) answer++;              
-        }
-        
+        answer = set.size();
         return answer;
-  
     }
 	//백트래킹
-	static void dfs(String s, String temp, int m) {
-            if(temp.length() == m){
-                int num = Integer.parseInt(temp);
-                if(!arr.contains(num)){
-                    arr.add(num);
-                }
-            }
+	static void dfs(String str, int idx) {
+        int num;
+            
+        if(str!=""){
+            num = Integer.parseInt(str);
+            if(isPrime(num)) set.add(num); // 소수판별
+        }
+        if(idx==arr.length) return; // 끝까지 다했을 때 
         
-            for(int i=0; i<s.length(); i++){
-                if(!check[i]){
-                    check[i] = true;
-                    temp += s.charAt(i);
-                    dfs(s, temp, m);
-                    check[i] = false;
-                    temp = temp.substring(0, temp.length()-1);
-                }
-            }
+        for(int i=0;i<arr.length;i++){
+            if(visited[i]) continue; // 방문한 노드면 넘어감
+            visited[i] = true; // 방문
+
+            dfs(str+arr[i], idx+1); // 방문 했을 시 재귀
+
+            visited[i] = false; // 백트래킹
+        }
 		
 	}
+    
 	//소수 판단
 	static boolean isPrime(int n) {
 		if(n<2) return false;
