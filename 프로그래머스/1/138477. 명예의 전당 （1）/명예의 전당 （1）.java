f51042ecc1907be991fc0ecdcfb0fc31fbbfec08
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-01-17 09:41:49 +0900

    [level 1] Title: 명예의 전당 (1), Time: 14.64 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 class Solution {
     public int[] solution(int k, int[] score) {
-
         int[] answer = new int[score.length];
         
         List<Integer> rank = new ArrayList<>();
@@ -12,11 +10,9 @@ class Solution {
         for (int i = 0; i < score.length; i++) {
  
             rank.add(score[i]);
-
             if (rank.size() > k) {
                 rank.remove(Collections.min(rank));
             }
-
             answer[i] = Collections.min(rank);
         }
         return answer;
