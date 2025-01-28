f38667600a00a5f0787a2132c56e2f1e9a82a4ad
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-01-28 09:32:30 +0900

    [level 1] Title: 소수 찾기, Time: 168.46 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+    	// 2는 소수이기 때문에 초기값 1로 선언
+        int answer = 1;
+        
+        // 3부터 n까지 반복하여 소수의 개수 찾기
+        for (int i = 3; i <= n; i++) {
+            if (isPrime(i)) answer += 1;
+        }
+        
+        return answer;
+    }
+    
+    private boolean isPrime(int num) {
+        
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
