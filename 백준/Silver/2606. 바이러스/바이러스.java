6492c5314432bd7f5b0f429f916acedb109e6ba1
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-02-10 09:19:25 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,52 +1,48 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,V,count;
+    static int N,V,count;
     static List<List<Integer>> adjList = new ArrayList<>();
     static boolean[] visit; // 중복 방문 방지
-    static boolean[][] matrix;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		V = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		
-		for (int i = 0; i < N + 1; i++) { // 0 dummy 
-			adjList.add(new ArrayList<Integer>());
-		}
-		
-		for (int i = 0; i < V ; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken());
-			int n2 = Integer.parseInt(st.nextToken());
-			adjList.get(n1).add(n2); 
-			adjList.get(n2).add(n1); 
-		}
-		
-        
-		visit = new boolean[N + 1];
-		
-		dfs(1);
-		System.out.println(count -1); // 1번 제외 감염된 컴퓨터 수
-	}
-	
-    
+        N = Integer.parseInt(br.readLine());
+        V = Integer.parseInt(br.readLine());
+
+
+        for (int i = 0; i < N + 1; i++) { // 0 dummy
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < V ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            adjList.get(n1).add(n2);
+            adjList.get(n2).add(n1);
+        }
+
+
+        visit = new boolean[N + 1];
+
+        dfs(1);
+        System.out.println(count -1); // 1번 제외 감염된 컴퓨터 수
+    }
+
+
     static void dfs(int n) { // n : 시작 정점
         visit[n] = true;
-             
+
         List<Integer> list = adjList.get(n);
         count++;
         for (Integer i : list) {
-			if( visit[i] ) continue;
+            if( visit[i] ) continue;
             dfs(i);
-		}         
+        }
     }
 }
