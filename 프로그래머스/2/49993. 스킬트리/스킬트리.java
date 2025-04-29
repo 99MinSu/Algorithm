89728cd64c3ab259438a8e4e7e5b33ec6e75b5f3
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-04-29 18:16:37 +0900

    [level 2] Title: 스킬트리, Time: 9.47 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        for (String cur : skill_trees) {	// skill_trees의 문자열 하나씩 보면서
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < cur.length(); i++) {	
-                if (skill.contains(cur.charAt(i) + ""))// 문자 순서대로 skill에 포함되는것만
-                    sb.append(cur.charAt(i));	// 따로 빼둠.
-            }
-            answer += skill.indexOf(sb.toString())==0 ? 1 : 0;
+        for (String cur : skill_trees) {
+            cur = cur.replaceAll("[^"+skill+"]", "");	//정규식으로 제거
+            answer += skill.indexOf(cur)==0 ? 1 : 0;
         }
         return answer;
     }
