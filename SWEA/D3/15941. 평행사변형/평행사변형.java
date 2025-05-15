268b7280f79011b9a98e191b4fda2a2f09b793ef
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-05-15 20:45:26 +0900

    [D3] Title: 평행사변형, Time: 76 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int N = Integer.parseInt(br.readLine());
+			
+			System.out.println("#" + test_case + " " + N*N);
+
+		}
+	}
+}
