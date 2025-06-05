a93b1d75a44241c59373f825ead64c3f2b089ea2
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-06-05 21:11:16 +0900

    [level 2] Title: 구명보트, Time: 11.12 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,12 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
+        
         int answer = 0;
+        
+        // 정렬 후 뒤에서 부터 체크
         Arrays.sort(people);
         
         int idx=0;
         for(int i=people.length-1;i>=idx;i--){
-            if(people[i]+people[idx]<=limit){
+            
+            if(people[i] + people[idx] <= limit){
                 answer++;
                 idx++;
             }
