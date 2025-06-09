87693d22f458822f02bd9f510d81f5234b3b2094
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-06-09 19:28:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 82.84 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -5,18 +5,20 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < s.length(); i++) {
-            String str = s.substring(i) + s.substring(0,i);
+            
+            String str = s.substring(i) + s.substring(0,i); // 돌려돌려
             Stack<Character> stack = new Stack<>();
             
             for (int j = 0; j < s.length(); j++) {
                 char c = str.charAt(j);
                 
-                if (stack.isEmpty()) {
+                if (stack.isEmpty()) { // 비어있으면 일단 넣는다
                     stack.push(c);
                     continue;
                 }
                 
-                if (c == ')' && stack.peek() == '(') {
+                // 괄호마다 한 쌍일 때 꺼낸다
+                if (c == ')' && stack.peek() == '(') { 
                     stack.pop();
                     continue;
                 } else if (c == '}' && stack.peek() == '{') {
@@ -27,7 +29,7 @@ class Solution {
                     continue;
                 }
                 
-                stack.push(c);
+                stack.push(c); // 아니면 푸쉬푸쉬
             }
 
             if (stack.size() == 0) {
