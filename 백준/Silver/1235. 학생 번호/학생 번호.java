22e7a0a46f0baae19afb241721c44885db88906f
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-06-17 20:14:42 +0900

    [Silver IV] Title: 학생 번호, Time: 124 ms, Memory: 16600 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int ans = 0;
+		String[] arr = new String[n];
+		HashSet<String> set = new HashSet<String>();
+		
+		for(int i=0; i<n; ++i) {
+			arr[i] = br.readLine();
+		}
+		for(int i=1; i<=arr[0].length(); ++i) {
+			for(int j=0; j<n; ++j) {
+				set.add(arr[j].substring(arr[j].length()-i));
+			}
+			if(set.size() == n) {
+				ans = i;
+				break;
+			}
+			set.clear();
+		}
+        System.out.println(ans);
+	}	
+}
