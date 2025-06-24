382ffcc288e4320066a15d514ca3def76a6cd68b
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-06-24 23:50:04 +0900

    [level 2] Title: 주식가격, Time: 55.96 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,7 +7,9 @@ public class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < prices.length; i++) {
+            
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                
                 answer[stack.peek()] = i - stack.peek();
                 stack.pop();  // 답을 구했기 때문에 stack에서 제거
             }
