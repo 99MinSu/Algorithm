31cf43795854211dc4c96d867e416d3a60fdbfa4
이민수 <89891084+99MinSu@users.noreply.github.com>
2025-07-01 22:35:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 632 ms, Memory: 81488 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -10,6 +10,7 @@ public class Main {
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
 		
+        //맵 2개
 		HashMap<Integer, Integer> ha1 = new HashMap<>();
 		HashMap<Integer, Integer> ha2 = new HashMap<>();
 		st = new StringTokenizer(br.readLine());
